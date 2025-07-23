4e5c28adeafae48dea8e95676c79513e4c1612d7
anhvlt-2k6 <anhvlt.2006@outlook.com>
2025-07-23 21:09:35 +0700

    .

---
+++ .temp/TestApp/src/testapp/TestApp.java
@@ -0,0 +1,22 @@
+package testapp;
+
+public class TestApp {
+
+    String id = "test";
+
+    
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public static void main(String[] args) {
+        TestApp ta = new TestApp();
+        ta.setId("Hi");
+        System.out.print(ta.getId());
+    }
+}
