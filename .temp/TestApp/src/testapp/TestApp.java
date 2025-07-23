c78a7715268e390c4ce4ff55ebca029a5b29ac17
anhvlt-2k6 <anhvlt.2006@outlook.com>
2025-07-23 22:51:55 +0700

    .

--- .temp/TestApp/src/testapp/TestApp.java
+++ .temp/TestApp/src/testapp/TestApp.java
@@ -4,25 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class TestApp {
-
-    String id = "test";
-
-    
-    
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-    
     public static void main(String[] args) {
-        TestApp ta = new TestApp();
-        ta.setId("Hi");
-        System.out.print(ta.getId());
-        
-        
-        
+        ArrayList<Integer> i = new ArrayList<>();
+        System.out.print(i.size());
     }
 }
